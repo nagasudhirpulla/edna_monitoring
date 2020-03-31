2210bc59ced062183834248085e8c51faa4d4275
nagasudhirpulla <nagasudhirpulla@gmail.com>
2020-03-31 19:23:31 +0530

    monitor lines

---
+++ src/EdnaMonitoring.Web/Pages/TransLines/Monitor.cshtml.cs
@@ -0,0 +1,26 @@
+﻿using System.Collections.Generic;
+using System.Threading.Tasks;
+using EdnaMonitoring.App.TransLines.Queries.GetAllActiveTransLines;
+using EdnaMonitoring.Domain.Entities;
+using MediatR;
+using Microsoft.AspNetCore.Mvc.RazorPages;
+
+namespace EdnaMonitoring.Web.Pages.TransLines
+{
+    public class MonitorModel : PageModel
+    {
+        private readonly IMediator _mediator;
+
+        public MonitorModel(IMediator mediator)
+        {
+            _mediator = mediator;
+        }
+
+        public IList<TransLine> TransLine { get; set; }
+
+        public async Task OnGetAsync()
+        {
+            TransLine = await _mediator.Send(new GetAllActiveTransLinesQuery());
+        }
+    }
+}
